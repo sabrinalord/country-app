1cfe43871cd6b2491cd985cc55be0e86460dc828
sabrinalord <sabrinalord@msn.com>
2021-08-10 20:53:07 +0100

    updated script filter feature
    added .filter function to display country name queries

--- script.js
+++ script.js
@@ -1,25 +1,48 @@
 
-const countryNameForm = document.getElementById('country-name-form')
-const urlBase = 'https://restcountries.eu/rest/v2/'
-let countryName = ""
 let countriesWrapper = document.querySelector('.countries-wrapper');
-
+const countryNameForm = document.getElementById('filter-form')
+let countryNameQuery = ""
 
 
 // ---------  Filters -------------------
 
-
-	
-countryNameForm.addEventListener("submit", event => {
-	event.preventDefault();
-	let country = countryNameForm.elements[0].value
-
-})
-
 async function getCountries() {
 	let response = await fetch('https://restcountries.eu/rest/v2/all');
-	let data = await response.json();	
-	return data;
+	let countriesData = await response.json();
+    
+return countriesData 
+
+}
+
+
+//take the countries data. If the query is empty, then display all. 
+//If it is not empty, only display the filtered results
+
+function DisplayAndFilterHTML(countriesData){
+let aCountry = countriesData[0];
+let filterResults = []
+
+// display all countries as default
+countriesWrapper.innerHTML = countriesData.map(createHTML).join('')
+// Event listener that stores user input
+	countryNameForm.addEventListener('submit', event => {
+		event.preventDefault();
+		countryNameQuery = document.getElementById('country-name').value
+		
+		filterResults = countriesData.filter(country => country.name.toUpperCase() === countryNameQuery.toUpperCase())
+         
+		//if array is empty 
+		
+		if (filterResults.length != 0) {
+			countriesWrapper.innerHTML = filterResults.map(createHTML).join('')
+		} else {
+			countriesWrapper.innerHTML = countriesData.map(createHTML).join('')
+		}
+
+
+
+	})
+
 
 }
 
@@ -33,9 +56,7 @@ return	`
 <img class ="flag-image" src="${aCountry.flag}" />
 </div>
 		<div class = "stat-container"> 
-			
-				
-				<p><span class ="subheading">Population:</span> ${aCountry.population}</p>
+				<p><span class ="subheading">Population:</span> ${aCountry.population.toLocaleString()}</p>
 				<p><span class ="subheading">Sub Region:</span> ${aCountry.subregion}</p>
 				<p><span class ="subheading">Languages:</span>
 					${aCountry.languages.map(language => {
@@ -54,12 +75,13 @@ return	`
 `
 }
 
-function displayHTML(countries){
-let aCountry = countries[20];
-	countriesWrapper.innerHTML = countries.map(createHTML).join('');
 
-}
 
-getCountries().then(displayHTML)
 
+// is search criteria is empty display all countries
+// else listen for a click and display countries based on search criteria
+
+//watchForm();
+
+getCountries().then(DisplayAndFilterHTML)
 
